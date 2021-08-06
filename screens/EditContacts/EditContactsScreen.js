d7ec21d3a34bc5411697436863b7fc791069b5eb
alexnick92 <alex.nicholas92@gmail.com>
2021-08-06 23:20:56 +0800

    json data test

--- screens/EditContacts/EditContactsScreen.js
+++ screens/EditContacts/EditContactsScreen.js
@@ -4,17 +4,50 @@ import {Card, Button, View, Text, TouchableOpacity} from 'react-native-ui-lib';
 
 import styles from './styles'
 
-import AntDesign from 'react-native-vector-icons/AntDesign';
-const plusIcon = <AntDesign name="pluscircleo" size={25} color="black" />;
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+const userIcon = <FontAwesome name="user-circle" size={100} color="orange" />;
 
 
 const EditContactsScreen = (props) => {
+
+  function renderHeader() {
+    return (
+      <View
+        row
+        paddingH-24
+        paddingV-14
+        center
+        style={{
+          backgroundColor: 'white',
+          shadowColor: '#000000',
+          shadowOffset: {width: 0, height: 6},
+          shadowOpacity: 0.09,
+          borderBottomWidth: 0.25,
+          borderColor: 'grey'
+        }}
+        >
+        <TouchableOpacity
+          onPress={() => navigation.navigate('Shop', {screen: 'Cart'})}
+          flex
+          row
+          spread
+          style={{}}>
+          <Text orange30>Cancel</Text>
+          <Text orange30 >Save</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
+
   
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#ffffff'}}>
-   
+          {renderHeader()}
       <ScrollView style={{backgroundColor: '#ffffff', paddingTop: 10}}>
-       <Text>Screen 2</Text>
+
+       <View flex center >
+       {userIcon} 
+       </View>
       </ScrollView>
     </SafeAreaView>
   );
