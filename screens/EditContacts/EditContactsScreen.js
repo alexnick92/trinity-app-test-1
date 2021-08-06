f74d761fcec846d5ec694015b31534432fc89d92
alexnick92 <alex.nicholas92@gmail.com>
2021-08-06 23:56:18 +0800

    end commit

--- screens/EditContacts/EditContactsScreen.js
+++ screens/EditContacts/EditContactsScreen.js
@@ -2,14 +2,12 @@ import React, {useState} from 'react';
 import {Image, ScrollView, SafeAreaView, Dimensions, Modal} from 'react-native';
 import {Card, Button, View, Text, TouchableOpacity} from 'react-native-ui-lib';
 
-import styles from './styles'
+import styles from './styles';
 
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 const userIcon = <FontAwesome name="user-circle" size={100} color="orange" />;
 
-
 const EditContactsScreen = ({navigation}) => {
-
   function renderHeader() {
     return (
       <View
@@ -23,9 +21,8 @@ const EditContactsScreen = ({navigation}) => {
           shadowOffset: {width: 0, height: 6},
           shadowOpacity: 0.09,
           borderBottomWidth: 0.25,
-          borderColor: 'grey'
-        }}
-        >
+          borderColor: 'grey',
+        }}>
         <TouchableOpacity
           onPress={() => navigation.goBack()}
           flex
@@ -33,24 +30,58 @@ const EditContactsScreen = ({navigation}) => {
           spread
           style={{}}>
           <Text orange30>Cancel</Text>
-          <Text orange30 >Save</Text>
+          <Text orange30>Save</Text>
         </TouchableOpacity>
       </View>
     );
   }
 
-  
   return (
     <SafeAreaView style={{flex: 1, backgroundColor: '#ffffff'}}>
-          {renderHeader()}
+      {renderHeader()}
       <ScrollView style={{backgroundColor: '#ffffff', paddingTop: 10}}>
+        <View flex center>
+          {userIcon}
+        </View>
+        <Card>
+          <Text bg-orange30 heading>
+            Main Information
+          </Text>
+          <View
+            style={styles.MainContainer}>
+            <Text text70R>First Name</Text>
+          </View>
+          <View style={styles.MainContainer} >
+          <Text text70R>Last Name</Text>
+          </View>
+        </Card>
 
-       <View flex center >
-       {userIcon} 
-       </View>
+        <Card>
+          <Text bg-orange30 heading>
+            Sub Information
+          </Text>
+          <View
+            style={styles.MainContainer}>
+            <Text text70R>Email</Text>
+          </View>
+
+          <View
+            style={styles.MainContainer}>
+            <Text text70R>Phone</Text>
+          </View>
+        </Card>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
 export default EditContactsScreen;
+
+
+
+
+
+
+
+
+
--- screens/EditContacts/styles.js
+++ screens/EditContacts/styles.js
@@ -1,30 +1,13 @@
-import { StyleSheet, Dimensions } from 'react-native';
+import {StyleSheet, Dimensions} from 'react-native';
 
 const styles = StyleSheet.create({
-MainContainer: {
-
-    },
-    Header: {
-        fontFamily: "Poppins-Regular" ,
-        fontSize: 32,
-
-        },
-    subHeader: {
-        fontFamily: "Poppins-Regular" ,
-        fontSize: 14,
-        },
-
-        CarouselStyle: {
-            width: '100%',
-            height: 400,
-            resizeMode: 'contain',
-            },
-
-        goToNext: {
-            color: '#1A4493',
-            fontSize: 15,
-            marginHorizontal: 5,
-        },
+  MainContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingRight: 32,
+    borderBottomWidth: 0.25,
+    borderColor: 'grey',
+  },
 });
 
 export default styles;
